cc22a2baa298253a28a5ae493fd48f339f3d329a
amruthabathula30 <54269250+amruthabathula30@users.noreply.github.com>
2019-08-21 15:05:04 +0530

    Update Runner22.java

--- src/test/java/runner22/Runner22.java
+++ src/test/java/runner22/Runner22.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Feature22",glue="stepdef22",plugin= {"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
+@CucumberOptions(json:target/cucumber.json)
 
 public class Runner22 {
 
