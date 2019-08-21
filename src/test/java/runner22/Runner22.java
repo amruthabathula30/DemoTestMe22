3dba5ab033d94224252553cc0785f97252e8e935
amruthabathula30 <54269250+amruthabathula30@users.noreply.github.com>
2019-08-21 15:13:12 +0530

    Update Runner22.java

--- src/test/java/runner22/Runner22.java
+++ src/test/java/runner22/Runner22.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(json:target/cucumber.json)
+@CucumberOptions(pluguin= {"json:target/cucumber.json"})
 
 public class Runner22 {
 
